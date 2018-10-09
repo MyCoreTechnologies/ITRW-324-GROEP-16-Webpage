45b0b7b2a30617cb44a6953a359d006af341207d
Jacques <27236285@student.g.nwu.ac.za>
2018-10-09 16:46:42 +0200

    Navigation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  // name = 'Jacques';
+  loadedFeature = 'login';
+  
+ onNavigate(feature: string){
+this.loadedFeature = feature;
+ }
 }
