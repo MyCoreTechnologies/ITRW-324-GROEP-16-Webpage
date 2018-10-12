20782fb0e688cacf1608ccdf7912452058654e76
Jacques <27236285@student.g.nwu.ac.za>
2018-10-12 15:50:45 +0200

    Report Student Back-End

--- src/app/reportstudent/reportstudent.component.ts
+++ src/app/reportstudent/reportstudent.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {subservice} from '../../post/web.service';
+import {NgForm} from '@angular/forms';
+import {Router,Routes,RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-reportstudent',
@@ -7,9 +10,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReportstudentComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router, private submitServe: subservice) {}
 
   ngOnInit() {
   }
+  addOffence:{};
 
+  data;
+
+  postOffences(form: NgForm) {
+    console.log(form.value);
+    this.submitServe.postOffence(form.value)
+    .subscribe(response => {
+      console.log(response);
+      if(sessionStorage.length < 1){
+        //@ts-ignore
+        this.data=response.body;
+        sessionStorage.setItem('data', this.data);
+      }else{
+        sessionStorage.clear();
+        //@ts-ignore
+        this.data=response.body;
+        sessionStorage.setItem('data', this.data);
+      }
+    },
+      (error) => console.log('Problem accuired during login.'));
+    } 
 }
