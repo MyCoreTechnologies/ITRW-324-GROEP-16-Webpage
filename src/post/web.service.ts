1389203d13f76e9fe2744b0f42c58a29779fcb6f
Jacques <27236285@student.g.nwu.ac.za>
2018-10-13 13:58:33 +0200

    Register back-End

--- src/post/web.service.ts
+++ src/post/web.service.ts
@@ -19,7 +19,7 @@ import {HttpClient, HttpHeaders} from '@angular/common/http';
             postBook(value)
             {
                     const header= new HttpHeaders().set('Content-Type', 'application/json');
-                    return this.httpclient.post('http://192.168.8.104:3000/book/addbook', value, {headers: header});
+                    return this.httpclient.post('http://192.168.8.103:3000/book/addbook', value, {headers: header});
             }
 
             getToken(){
@@ -35,7 +35,7 @@ import {HttpClient, HttpHeaders} from '@angular/common/http';
             postRegister(value)
             {
                 const header= new HttpHeaders().set('Content-Type', 'application/json');
-                return this.httpclient.post('http://localhost:3000/student/register', value, {});
+                return this.httpclient.post('http://192.168.8.103:3000/student/register', value, {});
             }
             getDisplayBookByName()
             {
@@ -45,7 +45,7 @@ import {HttpClient, HttpHeaders} from '@angular/common/http';
             postOffence(value)
             {
                 const header= new HttpHeaders().set('Content-Type', 'application/json');
-                return this.httpclient.post('http://localhost:3000/student_offence/reportOffence', value, {headers: header});
+                return this.httpclient.post('http://192.168.8.103:3000/student_offence/reportOffence', value, {});
                 
             }
     }
